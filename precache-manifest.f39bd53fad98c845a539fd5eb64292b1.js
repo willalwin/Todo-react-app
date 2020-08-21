7a52b467724d1998de1339324111b3c3a1dd1fbd
Will Alwin <willalwin@msn.com>
2020-08-21 15:24:00 -0700

    Updates

---
+++ precache-manifest.f39bd53fad98c845a539fd5eb64292b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c3cf94190ea7b67c88981ebf067db91",
+    "url": "/Todo-react-app/index.html"
+  },
+  {
+    "revision": "a2f5df217a75cf964665",
+    "url": "/Todo-react-app/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "79b994cc758cfe84ace1",
+    "url": "/Todo-react-app/static/js/2.54474cce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Todo-react-app/static/js/2.54474cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2f5df217a75cf964665",
+    "url": "/Todo-react-app/static/js/main.d802b459.chunk.js"
+  },
+  {
+    "revision": "11b3670fce8c8f75fef8",
+    "url": "/Todo-react-app/static/js/runtime-main.115fe61d.js"
+  }
+]);
